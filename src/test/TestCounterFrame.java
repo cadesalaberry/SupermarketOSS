a55943ec1f3fafa8f5a3c7c3e438a25376802efb
C-A de Salaberry <cadesalaberry@yahoo.com>
2013-11-18 14:23:57 -0500

    Add: First try with easy mock.

--- src/test/TestCounterFrame.java
+++ src/test/TestCounterFrame.java
@@ -1,11 +1,15 @@
 package test;
 
+import org.easymock.EasyMock;
+
+import Counter.MarketProxy;
 import CounterUI.CounterFrame;
 
 public class TestCounterFrame {
 
 	public static void main(String[] args) {
-		CounterFrame counter = new CounterFrame(new MarketProxyStub());
+		MarketProxy marketProxy = EasyMock.createNiceMock(MarketProxy.class);
+		CounterFrame counter = new CounterFrame(marketProxy);
 		counter.setVisible(true);
 	}
 
